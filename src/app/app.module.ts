93b49bfdb857e24c10376ef32bc5d3e05ad57de3
davilnv <davilima871@gmail.com>
2023-07-16 21:34:33 -0300

    Make the CSS for login forms and remove components for login button and welcome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { AuthContentComponent } from './auth-content/auth-content.component';
 import { WelcomeContentComponent } from './welcome-content/welcome-content.component';
 import { LoginFormComponent } from './login-form/login-form.component';
@@ -14,7 +13,6 @@ import { ButtonsComponent } from './buttons/buttons.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     AuthContentComponent,
     WelcomeContentComponent,
     LoginFormComponent,
